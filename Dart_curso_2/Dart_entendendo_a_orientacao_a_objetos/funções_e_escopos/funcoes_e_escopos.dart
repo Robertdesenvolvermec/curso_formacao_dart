34975682e7041dc64fc020b216246ad43778d240
Robert dos Santos Silva <robertsanttos.rs@gmail.com>
2022-09-22 22:36:29 -0300

    Classes abstrata e polimorfismo

--- Dart_curso_2/Dart_entendendo_a_orientacao_a_objetos/funções_e_escopos/funcoes_e_escopos.dart
+++ Dart_curso_2/Dart_entendendo_a_orientacao_a_objetos/funções_e_escopos/funcoes_e_escopos.dart
@@ -8,10 +8,22 @@ void main() {
   int diasDesdeColheita = 4;
   bool isMadura = funcEstaMaduro(diasDesdeColheita);
 
-  Fruta fruta1 = Fruta('Uva', 40.0, 'Roxo', 'Doce', 40);
+  Fruta Uva = Fruta('Uva', 40.0, 'Roxo', 'Doce', 50);
+  Uva.printAlimento();
+  Uva.estaMadura(50);
+  Uva.separarIngredientes();
+  Uva.fazerMassa();
+  Uva.assar();
 
-  fruta1.estaMadura(20);
-  fruta1.fazerSuco();
+  Legume Cenoura = Legume('Cenoura', 100.00, 'laranja', true);
+
+  Nozes CastanhaDoPara =
+      Nozes('Castanha do Pará', 20.0, 'marrom claro', 'seca', 30, true, 30.0);
+  CastanhaDoPara.printAlimento();
+  CastanhaDoPara.estaMadura(20);
+  CastanhaDoPara.separarIngredientes();
+  CastanhaDoPara.fazerMassa();
+  CastanhaDoPara.assar();
 }
 
 void funcMostraFruta(String nome, int dias, {double? kg}) {
@@ -46,6 +58,7 @@ bool funcEstaMaduro(int dias) {
   }
 }
 
+// classes para criar alimentos com
 class Alimento {
   String nome;
   double peso;
@@ -54,11 +67,11 @@ class Alimento {
   Alimento(this.nome, this.peso, this.cor);
 
   printAlimento() {
-    print('Este(a) $nome pesa $peso kg e é $cor');
+    print('Este(a) $nome pesa $peso kg e é da cor $cor');
   }
 }
 
-class Fruta extends Alimento {
+class Fruta extends Alimento implements Bolo {
   String sabor;
   int diasDesdeColheita;
   bool? isMadura;
@@ -69,17 +82,36 @@ class Fruta extends Alimento {
       : super(nome, peso, cor);
 
   estaMadura(int diasParaMadura) {
-    isMadura = diasDesdeColheita >= diasParaMadura;
-    print(
-        'A fruta $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura dias para poder comer. Ela está madura? $isMadura');
+    if (isMadura = diasDesdeColheita >= diasParaMadura) {
+      print(
+          'A fruta $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura dias para poder comer. Ela está madura? $isMadura');
+    } else {
+      print(
+          'A fruta $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura dias para poder comer. Ela está madura? $isMadura');
+    }
   }
 
   fazerSuco() {
     print('Você fez um suco de $nome');
   }
+
+  @override
+  void separarIngredientes() {
+    print('Colher a fruta');
+  }
+
+  @override
+  void fazerMassa() {
+    print('Bater a fruta com a farinha, fermento, açúcar e outros...');
+  }
+
+  @override
+  void assar() {
+    print('Colocar no forno');
+  }
 }
 
-class Legume extends Alimento {
+class Legume extends Alimento implements Bolo {
   bool isPrecisaCozinhar;
 
   Legume(String nome, double peso, String cor, this.isPrecisaCozinhar)
@@ -91,6 +123,21 @@ class Legume extends Alimento {
     } else {
       print('O $nome não precisa cozinhar, pode comer!');
     }
+  }
+
+  @override
+  void assar() {
+    // TODO: implement assar
+  }
+
+  @override
+  void fazerMassa() {
+    // TODO: implement fazerMassa
+  }
+
+  @override
+  void separarIngredientes() {
+    // TODO: implement separarIngredientes
   }
 }
 
@@ -121,4 +168,22 @@ class Nozes extends Fruta {
   Nozes(String nome, double peso, String cor, String sabor,
       int diasDesdeColheita, bool isMadura, this.porcentagemOleoNatural)
       : super(nome, peso, cor, sabor, diasDesdeColheita);
+
+  @override
+  void fazerMassa() {
+    print('Tirar a casca');
+    super.fazerMassa();
+  }
+}
+
+// classes abstratas para criar outros tipos de alimentos
+abstract class Bolo {
+  //separar os ingredientes
+  void separarIngredientes();
+
+  //faça a massa
+  void fazerMassa();
+
+  //assar
+  void assar();
 }
